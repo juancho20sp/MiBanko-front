b6386d4020107c0c5337a10b15f4ac6de175ff0f
JCPosso <juancamiloposso01@gmail.com>
2022-05-03 22:03:30 -0500

    update: add validation confirm password , email and password

--- src/hooks/validateInfo.js
+++ src/hooks/validateInfo.js
@@ -1,9 +1,27 @@
+function isStrong(password) {
+    var rules = {
+        'capital' : /[A-Z]/,
+        'digit'   : /[0-9]/,
+        'except'  : /[aeiou]/,
+        'all'    : /^[@#][A-Za-z0-9]{7,14}$/
+    };
+    return rules.capital.test(password) && rules.digit.test(password) && !rules.except.test(password) && 
+          rules.all.test(password);
+}
 export default function validateInfo(values) {
     let errors = {};  
     if (!values.user) {
       errors.user = 'Email required';
     } else if (!/\S+@\S+\.\S+/.test(values.user)) {
       errors.user = 'Email address is invalid';
+    }
+    if (!values.password) {
+        errors.password = 'Password required';
+      } else if (!isStrong(values.password)) {
+        errors.password = 'This password is weak!';
+      }
+    if (values.confirm_password && values.confirm_password!==values.password ) {
+      errors.confirm_password = 'Confirm password is invalid';
     }
     return errors;
   }
--- src/pages/crearUsuario/index.js
+++ src/pages/crearUsuario/index.js
@@ -3,39 +3,15 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import axios from "axios";
+import validate from '../../hooks/validateInfo';
+import useForm from './useForm';
+
 
 const CrearCuenta = () => {
-  let history = useNavigate();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    let location = "api/v1/users";
-    const data = new FormData(event.currentTarget);
-    let body = {};
-    body = {
-      user: {
-        user_name: data.get("user_name"),
-        user_lastname: data.get("user_lastname"),
-        document_type: data.get("document_type"),
-        document_number: Number(data.get("document_number")),
-        role: "CLIENT",
-        username: data.get("username"),
-        email: data.get("email"),
-        password: data.get("password"),
-      },
-    };
+  const { handleChange, handleSubmit, values, errors } = useForm(
+    validate
+  );
 
-    axios
-      .post(window.$dir + location + `/createUser`, body)
-      .then(function (response) {
-        if (response.status === 200) {
-          Swal.fire("Creado correctamente", "success");
-          history('/');
-        } else {
-          Swal.fire("Something is Wrong :(!", "try again later", "error");
-        }
-      });
-    event.target.reset();
-  };
   return (
     <div className="form-content">
       <form onSubmit={handleSubmit} className="form">
@@ -61,7 +37,14 @@ const CrearCuenta = () => {
             <option value="PS">P.S</option>
           </TipoDeDocumento>
           <Usuario name="username" placeholder="Usuario" required/>
-          <Email name="email" placeholder="Email" required/>
+          <Email 
+            name="user" 
+            placeholder="Email" 
+            required
+            value={values.user}
+            onChange={handleChange}
+          />
+           {errors.user && <AlertEmail>{errors.user}</AlertEmail>}
           <Apellido name="user_lastname" placeholder="Apellido" required/>
           <NúmeroDeDocumento
             name="document_number"
@@ -72,12 +55,18 @@ const CrearCuenta = () => {
             name="password"
             placeholder="Contraseña"
             type="password" required
+            value={values.password}
+            onChange={handleChange}
           />
+          {errors.password && <AlertPassword>{errors.password}</AlertPassword>}
           <ConfirmarContraseña
             name="confirm_password"
             placeholder="ConfirmarContraseña"
-            type="password"
+            type="password" required
+            value={values.confirm_password}
+            onChange={handleChange}
           />
+          {errors.confirm_password && <AlertPasswordConfirm>{errors.confirm_password}</AlertPasswordConfirm>}
           <Rectangle3 />
           <CrearCuentaBtn>Crear cuenta</CrearCuentaBtn>
         </RootWrapperCrearCuenta>
@@ -86,6 +75,36 @@ const CrearCuenta = () => {
   );
 };
 export default CrearCuenta;
+const AlertEmail = styled.p`
+background: #F00741;
+font-size: 25px;
+font-family: Roboto, sans-serif;
+font-weight: 400;
+text-align: left;
+position: absolute;
+left: 795px;
+top: 590px;
+`;
+const AlertPassword = styled.p`
+background: #F00741;
+font-size: 25px;
+font-family: Roboto, sans-serif;
+font-weight: 400;
+text-align: left;
+position: absolute;
+left: 202px;
+top: 700px;
+`;
+const AlertPasswordConfirm = styled.p`
+background: #F00741;
+font-size: 25px;
+font-family: Roboto, sans-serif;
+font-weight: 400;
+text-align: left;
+position: absolute;
+left: 802px;
+top: 700px;
+`;
 const RootWrapperCrearCuenta = styled.div`
   min-height: 100vh;
   background-color: rgba(255, 255, 255, 1);
